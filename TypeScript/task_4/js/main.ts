3068d06fcb4f2b742f3d3a0085a92cdc01d7225e
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-05-31 17:30:05 -0700

    main.ts

--- TypeScript/task_4/js/main.ts
+++ TypeScript/task_4/js/main.ts
@@ -1,53 +1,24 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string; 
-  fullTimeEmployee: boolean;
-  location: string;
-  yearsOfExperience?: number;
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  [propName: string]: any;
-  /* eslint-enable @typescript-eslint/no-explicit-any */
-}
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
 
-//Extending the Teacher class
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Milan",
+  lastName: "Carevic",
+  experienceTeachingC: 22,
+};
 
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
+console.log("C++");
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
+console.log("Java");
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-export const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`
-}
-
-
-interface StudentConstructor {
-    new(firstName: string, lastName: string): StudentClassInterface;
-}
-    
-interface StudentClassInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-    
-export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
-  firstName: string;
-  lastName: string;
-    
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-    
-  workOnHomework(): string {
-    return "Currently working";
-  }
-    
-  displayName(): string {
-    return this.firstName;
-  }
-}
+console.log("React");
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
