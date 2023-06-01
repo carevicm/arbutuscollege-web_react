683a7deaf9a0132e6ae8a1a7408f266fc57db46e
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-05-31 17:41:36 -0700

    Java.ts

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,16 @@
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingJava?: number;
+    }
+  
+    export class Java extends Subject {
+      getRequirements(): string {
+        return 'Here is the list of requirements for Java';
+      }
+  
+      getAvailableTeacher(): string {
+        if (!this.teacher.experienceTeachingReact) return 'No available teacher';
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+    }
+  }
