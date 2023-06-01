d42727115d0ae5e51030e476a5c163b20bc347ee
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-05-31 17:44:29 -0700

    main.ts

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,53 +1,33 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string; 
-  fullTimeEmployee: boolean;
-  location: string;
-  yearsOfExperience?: number;
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  [propName: string]: any;
-  /* eslint-enable @typescript-eslint/no-explicit-any */
+interface MajorCredits {
+  credits: number;
+  _brand: 'MajorCredits';
 }
 
-//Extending the Teacher class
-
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface MinorCredits {
+  credits: number;
+  _brand: 'MinorCredits';
 }
 
+const sumMajorCredits = (
+  subject1: MajorCredits,
+  subject2: MajorCredits,
+): MajorCredits => {
+  const result: MajorCredits = {
+    credits: subject1.credits + subject2.credits,
+    _brand: 'MajorCredits',
+  };
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
+  return result;
+};
 
-export const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`
-}
+const sumMinorCredits = (
+  subject1: MinorCredits,
+  subject2: MinorCredits,
+): MinorCredits => {
+  const result: MinorCredits = {
+    credits: subject1.credits + subject2.credits,
+    _brand: 'MinorCredits',
+  };
 
-
-interface StudentConstructor {
-    new(firstName: string, lastName: string): StudentClassInterface;
-}
-    
-interface StudentClassInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-    
-export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
-  firstName: string;
-  lastName: string;
-    
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-    
-  workOnHomework(): string {
-    return "Currently working";
-  }
-    
-  displayName(): string {
-    return this.firstName;
-  }
-}
+  return result;
+};
